5e6ba3fb898468d1957b942eec3d38ab234f2fd6
Adarsh singh <adarshsingh3384@gmail.com>
2026-09-05 11:18:09 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (36.78%) - LeetSync

--- 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
+++ 4284-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
-        int n=nums.size();
-        int smallIdx=INT_MAX;
-        int maxi=-1;
-        vector<int>mini(n);
-        mini[n-1]=nums[n-1];
-        for(int i=n-2;i>=0;i--){
-            mini[i]=min(nums[i],mini[i+1]);
+          int n = nums.size();
+        vector<int>suffixMin(n);
+        suffixMin[n - 1] = nums[n - 1];
+
+        for(int i=n-2; i>=0; i--){
+            suffixMin[i] = min(nums[i], suffixMin[i+1]);
         }
-        for(int i=0;i<n;i++){
-            maxi=max(maxi,nums[i]);
-            cout<<maxi<<" ";
-            int mn=mini[i];
-            cout<<mn<<" ";
-            if(maxi-mn<=k && i<smallIdx){
-                smallIdx=i;
+        vector<int>prefixMax(n);
+        prefixMax[0] = nums[0];
+        for(int j =1; j<n; j++){
+            prefixMax[j] = max(nums[j], prefixMax[j-1]);
+        }
+
+        for(int i =0; i<n; i++){
+            int score = prefixMax[i] - suffixMin[i];
+            if(score <= k){
+                return (i);
             }
-        }
-        return smallIdx==INT_MAX?-1:smallIdx;
-        
+        } 
+        return -1;
     }
 };
